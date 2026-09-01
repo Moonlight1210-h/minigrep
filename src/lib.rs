4cd83e2e1e36a0f2bcdc776a3c0946ac8288abfe
Hussein Alshmary <qwex1210@gmail.com>
2026-09-01 18:34:22 +0300

    complete clapintegration and CLI  improvment

--- src/lib.rs
+++ src/lib.rs
@@ -1,29 +1,47 @@
 use clap::Parser;
 use std::error::Error;
+use std::fs;
 pub type MyResult<T> = Result<T, Box<dyn Error>>;
 /// A line orinted search tool
 #[derive(Parser, Debug)]
 #[command(version,about,long_about = None)]
 
-struct Config {
+pub struct Config {
     /// The pattern to search for
     #[arg(short = 'q', long = "query")]
-    query: String,
+    pub query: String,
 
     /// The file path
-    #[arg(short = 'f', long = "file_path", default_value = None)]
-    file_path: String,
+    #[arg(short = 'f', long = "file_path")]
+    pub file_path: String,
 
     /// case sensitive search
-    #[arg(short = 'i', default_value = "false")]
-    case_sensitive: bool,
+    #[arg(short = 'i' ,long = "case_insensitive")]
+    pub case_sensitive: bool,
 }
 
-fn get_args() -> MyResult<Config> {
+ 
+
+pub fn get_args() -> MyResult<Config> {
     Ok(Config::parse())
 }
 
 /// The core logic fun
 pub fn run(config: Config) -> MyResult<()> {
-    todo!();
+    let mut vectory: Vec<String> = Vec::new();
+    let to_read = fs::read_to_string(config.file_path)?;
+    for line in to_read.lines() {
+        if config.case_sensitive {
+            if line.contains(&config.query ) {
+                vectory.push(line.to_string());
+            }
+        } else {
+            if line.to_lowercase().contains(&config.query.to_lowercase()) {
+                vectory.push(line.to_string());
+            }
+        }
+    }
+   
+    println!("{:#?}", vectory);
+    Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,11 @@
+use minigrep::{get_args, run};
+use std::process;
 
+fn main() {
+    let config = get_args().unwrap();
+
+    if let Err(e) = run(config) {
+        eprintln!("there was an error runing the applecation {e}");
+        process::exit(1);
+    }
+}
