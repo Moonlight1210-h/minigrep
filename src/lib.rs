ae4b6693db2b6374e8ad3c3837be37cafea00d8b
Hussein Alshmary <qwex1210@gmail.com>
2026-09-02 10:26:25 +0300

    adding the -n flag

--- src/lib.rs
+++ src/lib.rs
@@ -16,6 +16,10 @@ pub struct Config {
     /// Perform case-insensitive search
     #[arg(short = 'i', long = "ignore-case")]
     pub ignore_case: bool,
+
+    /// Show_line_numbers
+    #[arg(short = 'n' , long = "line_number")]
+   pub show_line_number : bool
 }
 
 pub fn get_args() -> MyResult<Config> {
@@ -26,7 +30,7 @@ pub fn run(config: Config) -> MyResult<()> {
     let mut results: Vec<String> = Vec::new();
     let content = fs::read_to_string(&config.file_path)?;
     for line in content.lines() {
-        if config.ignore_case {
+        if config.ignore_case  {
             if line.to_lowercase().contains(&config.query.to_lowercase()) {
                 results.push(line.to_string());
             }
@@ -36,9 +40,16 @@ pub fn run(config: Config) -> MyResult<()> {
             }
         }
     }
-
-    for line in results {
+     if config.show_line_number {
+        for (line_num,line) in results.iter().enumerate(){
+            println!("{}. {}",line_num+1 ,line)
+        } 
+     }
+     else {   for line in results {
+        
         println!("{}", line);
-    }
+    }}
+ 
     Ok(())
 }
+ 
